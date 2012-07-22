142b50d7832fdb61d07fa78b29951e6c8d60b12d
lynnfaraday <lomeara@gmail.com>
2012-07-22 10:23:46 -0400

    Removing the unnecessary to page method

--- lib/ruhoh/compilers/pagination.rb
+++ lib/ruhoh/compilers/pagination.rb
@@ -7,18 +7,11 @@ class Ruhoh
         posts_db['pagination'].each do |p|             
           File.open(target + "/index#{p['page_number']}.html", 'w:UTF-8') do |index_file|
             page.data['layout'] = "index_page"
-            p['posts'] = self.to_posts(p['posts'], posts_db)
             page.data['pagination'] = p
             index_file.puts page.render
           end
         end
       end # self.run
-      
-      def self.to_posts(abbreviated_posts, posts_db)
-         Array(abbreviated_posts).map { |id|
-             posts_db['dictionary'][id]
-          }.compact
-      end
     end # Pagination
   end # Compiler
 end # Ruhoh

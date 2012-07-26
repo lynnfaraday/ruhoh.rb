b58fc5d50151ebbac69ef605f56fa1acb6d1a1f7
lynnfaraday <lomeara@gmail.com>
2012-07-26 11:37:16 -0400

    Added support for showing when page indices are truncated

--- lib/ruhoh/parsers/pagination.rb
+++ lib/ruhoh/parsers/pagination.rb
@@ -3,6 +3,9 @@ class Ruhoh
     module Posts
 
       class Paginator
+        
+        PagesToShowBeforeAndAfter = 3
+        
         def initialize(posts_per_page, posts)
           @posts_per_page = posts_per_page
           @posts = posts
@@ -18,13 +21,14 @@ class Ruhoh
 
         def paginate
           total_posts = @posts.count
+          
           @pages = []
           if (@posts_per_page == nil)
             @pages << build_page_metadata(0, 1, total_posts, @posts)
           else
             total_pages = (total_posts.to_f / @posts_per_page).ceil
             (0..total_pages - 1).each do |page_index|
-              current_page_posts = @posts[page_index * @posts_per_page, @posts_per_page]        
+              current_page_posts = @posts[page_index * @posts_per_page, @posts_per_page]   
               @pages << build_page_metadata(page_index, total_pages, total_posts, current_page_posts)
             end
           end
@@ -39,20 +43,58 @@ class Ruhoh
             'total_posts'   => total_posts,
             'per_page'      => @posts_per_page,
             'posts'         => posts.map { |p| p['id'] },       
-            'previous_page' => get_prev_index_page(page_number),
-            'next_page'     => get_next_index_page(page_number, total_pages)
+            'prev'          => prev_indices(page_number),
+            'next'          => next_indices(page_number, total_pages),
+            'prev_truncated'=> prev_truncated(page_number),
+            'next_truncated'=> next_truncated(page_number, total_pages)
+            
           }
         end
-        
-        def get_prev_index_page(page_number)
+                
+        def prev_indices(page_number)
           return nil if page_number == 1
-          return "/" if page_number == 2
-          return "/index/#{page_number - 1}/"
+          prev_start = page_number - PagesToShowBeforeAndAfter
+          prev = []
+          for p in (prev_start..page_number - 1)
+            if (p < 1)
+              next
+            end
+            prev << { 'page_number' => p, 'url' => index_page_url(p) }
+          end
+          prev
+        end
+        
+        def prev_truncated(page_number)
+          prev = page_number - PagesToShowBeforeAndAfter - 1
+          prev > 0 ? index_page_url(prev) : nil
+        end
+        
+        
+        def next_indices(page_number, total_pages)
+          return nil if page_number == total_pages
+          next_end = page_number + PagesToShowBeforeAndAfter
+          next_pages = []
+          for p in (page_number + 1..next_end)
+            if (p > total_pages)
+              next
+            end
+            next_pages << { 'page_number' => p, 'url' => index_page_url(p) }
+          end
+          next_pages
+        end
+        
+        def next_truncated(page_number, total_pages)
+          next_shown = page_number + PagesToShowBeforeAndAfter + 1
+          next_shown <= total_pages  ? index_page_url(next_shown) : nil
         end
 
         def get_next_index_page(page_number, total_pages)
           return nil if page_number == total_pages
-          return "/index/#{page_number + 1}/"
+           index_page_url(page_number + 1)
+        end
+        
+        def index_page_url(page_number)
+          page_number == 1 ? "/" : "/index/#{page_number}/"
         end
 
       end

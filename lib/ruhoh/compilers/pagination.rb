553b21dc1d85a7b6a11b142bb06b20b845c4bcb5
lynnfaraday <lomeara@gmail.com>
2012-08-05 20:05:23 -0400

    Made pagination output less spammy.

--- lib/ruhoh/compilers/pagination.rb
+++ lib/ruhoh/compilers/pagination.rb
@@ -10,13 +10,13 @@ class Ruhoh
         base_dir = target + "/index"
         FileUtils.mkdir_p base_dir
 
+        Ruhoh::Friend.say { green "Creating pagination files." }
+
         Ruhoh::DB.payload['db']['posts']['pagination']['index_pages'].each do |p|  
             dir = "#{base_dir}/#{p['page_number']}"
             FileUtils.mkdir_p dir
             file_name = "#{dir}/index.html"
-          
-          Ruhoh::Friend.say { green "processed: #{file_name}" }          
-          
+                    
           File.open(file_name, 'w:UTF-8') do |file|
             page.data['pagination'] = p
             file.puts page.render
